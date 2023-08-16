8ced78752ff6d29ac77b8b25e2819b458aea076a
Gorasiya Dhruv <gorasiyadhruv021@gmail.com>
2023-08-16 18:17:23 +0530

    some changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,8 +9,13 @@ import { CheckBoxComponent } from './check-box/check-box.component';
 import { ChipsComponent } from './chips/chips.component';
 import { CoreComponent } from './core/core.component';
 import { DatePickerComponent } from './date-picker/date-picker.component';
+import { AutocompleteComponent } from './autocomplete/autocomplete.component';
 
 const routes: Routes = [
+  {
+    path:'Autocomplete',
+    component:AutocompleteComponent
+  },
   {
     path:'Badge',
     component:BadgeComponent
